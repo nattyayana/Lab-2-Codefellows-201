4724d9ba947afbb0b494d12e0e1a95dbb4039547
Alexander <aloecend@gmail.com>
2018-03-06 13:59:30 -0800

    added question prompts to app.js

---
+++ js/app.js
@@ -0,0 +1,11 @@
+'use strict';
+
+var nameAnswer = prompt('Is my name Alex?').toLowerCase();
+
+var birthAnswer = prompt('Was I born in Washington state?').toLowerCase();
+
+var travelAnswer = prompt('Have I travelled internationally?').toLowerCase();
+
+var roleplayingAnswer = prompt('Is D&D my favorite roleplaying system?').toLowerCase();
+
+var numCatsAnswer = prompt('Does my family have only one cat?').toLowerCase();
