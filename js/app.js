ef1286ad03eec33b79fc5b3ca2c5b47489fe3d0c
Alexander Stone <aloecend@gmail.com>
2018-03-07 14:59:07 -0800

    Removed questions and added skeleton for handling questions as arrays in a for loop.

--- js/app.js
+++ js/app.js
@@ -12,172 +12,42 @@
 //   }
 // }
 
-const numQuestions = 7;
 var correctQuestions = 0;
 
-var correctAnswer = false;
-while (!correctAnswer){
-  var nameAnswer = prompt('Is my name Alex?').toLowerCase();
-  let firstChar = nameAnswer.charAt(0);
-  if (firstChar === ('y') || firstChar === ('n')){
-    correctAnswer = true;
-  }
-  else {
-    alert('That is not a possible answer');
-    correctAnswer = false;
-  }
-}
-
-if (nameAnswer === 'y'){
-  alert('Correct! Legally my name is Alexander, but I\'ve literally never been called that... ever');
-  correctQuestions++;
-}
-else if (nameAnswer === 'n'){
-  alert('Wrong, my name is in fact Alex.');
-}
-else {
-  console.log('How did you get here??? Invalid User Input got past the validation function.');
-}
-
-correctAnswer = false;
-while (!correctAnswer) {
-  var birthAnswer = prompt('Was I born in Washington state?').toLowerCase();
-  let firstChar = birthAnswer.charAt(0);
-  if (firstChar === ('y') || firstChar === ('n')){
-    correctAnswer = true;
-  }
-  else {
-    alert('That is not a possible answer');
-    correctAnswer = false;
-  }
-}
-if (birthAnswer === 'y') {
-  alert('Wrong, I was born in California.');
-}
-else if (birthAnswer === 'n') {
-  alert('Correct, I was born in California.');
-  correctQuestions++;
-}
-else {
-  console.log('How did you get here??? Invalid User Input got past the validation function.');
-}
-
-correctAnswer = false;
-while(!correctAnswer){
-  var travelAnswer = prompt('Have I travelled internationally?').toLowerCase();
-  let firstChar = travelAnswer.charAt(0);
-  if (firstChar === ('y') || firstChar === ('n')){
-    correctAnswer = true;
-  }
-  else {
-    alert('That is not a possible answer');
-    correctAnswer = false;
-  }}
-if (travelAnswer === 'y') {
-  alert('Correct! I have travelled internationally many times');
-  correctQuestions++;
-}
-else if (nameAnswer === 'n') {
-  alert('Wrong, I have been outside the United States many times');
-}
-else {
-  console.log('How did you get here??? Invalid User Input got past the validation function.');
-}
-
-correctAnswer = false;
-while(!correctAnswer){
-  var roleplayingAnswer = prompt('Is D&D my favorite roleplaying system?').toLowerCase();
-  let firstChar = roleplayingAnswer.charAt(0);
-  if (firstChar === ('y') || firstChar === ('n')){
-    correctAnswer = true;
-  }
-  else {
-    alert('That is not a possible answer');
-    correctAnswer = false;
-  }
-}
-if (roleplayingAnswer === 'y') {
-  alert('Wrong, D&D is one of my least favorite roleplaying systems I have actually played.');
-}
-else if (roleplayingAnswer === 'n') {
-  alert('Correct, I dont really like D&D very much.');
-  correctAnswer++;
-}
-else {
-  console.log('How did you get here??? Invalid User Input got past the validation function.');
-}
-
-correctAnswer = false;
-while(!correctAnswer){
-  var numCatsAnswer = prompt('Does my family have only one cat?').toLowerCase();
-  let firstChar = numCatsAnswer.charAt(0);
-  if (firstChar === ('y') || firstChar === ('n')){
-    correctAnswer = true;
-  }
-  else {
-    alert('That is not a possible answer');
-    correctAnswer = false;
-  }
-}
-if (numCatsAnswer === 'y') {
-  alert('Wrong, there are five total cats in my immediate family, three in Washington and two in California.');
-}
-else if (nameAnswer === 'n') {
-  alert('Correct, there are five total cats in my immediate family, three in Washington and two in California.');
-  correctQuestions++;
-}
-else {
-  console.log('How did you get here??? Invalid User Input got past the validation function.');
-}
-
-correctAnswer = false;
-while(!correctAnswer){
-  var numDogsAnswer = parseInt(prompt('How many dogs does my immediate family have?'));
-  if (isNaN(numDogsAnswer)){
-    alert('That is not a possible answer.');
-    correctAnswer = false;
-  }
-  else{
-    correctAnswer = true;
-    if (numDogsAnswer === 0){
-      alert('Correct, my immediate family owns no dogs and hasn\'t owned any dogs for as longs as I have been alive');
-      correctQuestions++;
-    }
-    else if (numDogsAnswer !== 0){
-      alert('Wrong, my immediate family owns no dogs and has never owned a dog while I have been alive.');
-    }
-    else {
-      console.log('Math has broken. Somehow there is something that is both not equal to a number and not not equal to a number.');
-    }
-  }
-}
-
+var questionArray = ['Is my name Alex?', 'Was I born in Washington state?', 'Have I travelled internationally?', 'Is D&D my favorite roleplaying system?', 'Does my immediate family have any cats?', 'How many dogs does my immediate family have?', 'Which one of the following countries have I never visited: Argentina, Australia, Canada, Italy, Sweden, Peru, United Kingdom'];
+var answerArray = ['y', 'n', 'y', 'n', 'n', '0', 'Sweden'];
+var answerTypeArray = ['text', 'text', 'text', 'text', 'text', 'number', 'country-array'];
 var countryArray = ['Argentina', 'Australia', 'Canada', 'Italy', 'Sweden', 'Peru', 'United Kingdom'];
-correctAnswer = false;
-while(!correctAnswer){
-  var neverCountry = prompt('Which one of the following countries have I never visited: Argentina, Australia, Canada, Italy, Sweden, Peru, United Kingdom');
-  for (let i = 0; i < countryArray.length; i++){
-    if (neverCountry === countryArray[i]){
-      correctAnswer = true;
-      if (countryArray[i] === 'Sweden'){
-        alert('Correct, I have never been to Sweden');
-        correctQuestions++;
-      }
-      else {
-        alert('Wrong, I have been to ' + countryArray[i] + '.');
-      }
-      break;
-    }
+
+for (let i = 0; i < questionArray.length; i++){
+  let correctFormat = false;
+  var userAnswer = prompt(questionArray[i]);
+
+  //check if userAnswer is in the correct format
+  if (answerTypeArray[i] === 'text'){
+
+  } 
+  else if (answerTypeArray[i] === 'number'){
+
   }
-  if (correctAnswer === false){
-    alert('That is not a valid answer');
+  else if (answerTypeArray[i] === 'country-array'){
+
+  }
+
+  //check userAnswer if correct format, otherwise reduce i by to compensate for the for loop auto increasing it
+  if (correctFormat){
+
+  }
+  else {
+    i--;
   }
 }
 
-if (correctQuestions !== numQuestions){
-  alert('You got ' + correctQuestions + ' questions out of ' + numQuestions + ' correct.');
+
+if (correctQuestions !== questionArray.length){
+  alert('You got ' + correctQuestions + ' questions out of ' + questionArray.length + ' correct.');
 }
-else if (correctQuestions === numQuestions){
+else if (correctQuestions === questionArray.length){
   alert('You got every question right.');
 }
 else {
