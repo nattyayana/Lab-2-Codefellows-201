86063e06094470540b17405e366f6ebdf7da453d
Alexander Stone <aloecend@gmail.com>
2018-03-07 13:55:05 -0800

    Replaced function calls with copied and pasted code from the function per new Lab requirements.

--- js/app.js
+++ js/app.js
@@ -1,21 +1,28 @@
 'use strict';
 
 //This function takes in a user Statement and checks to see if it is in the correct format(in this case some variant of yes or no). That statement will have already been converted to lower case.
-function isCorrectFormat(statement){
-  var firstChar = statement.charAt(0);
-  if (firstChar === ('y') || firstChar === ('n')){
-    return true;
-  }
-  else {
-    alert('That is not a possible answer');
-    return false;
-  }
-}
+// function isCorrectFormat(statement){
+//   var firstChar = statement.charAt(0);
+//   if (firstChar === ('y') || firstChar === ('n')){
+//     return true;
+//   }
+//   else {
+//     alert('That is not a possible answer');
+//     return false;
+//   }
+// }
 
 var correctAnswer = false;
 while (!correctAnswer){
   var nameAnswer = prompt('Is my name Alex?').toLowerCase();
-  correctAnswer = isCorrectFormat(nameAnswer);
+  let firstChar = nameAnswer.charAt(0);
+  if (firstChar === ('y') || firstChar === ('n')){
+    correctAnswer = true;
+  }
+  else {
+    alert('That is not a possible answer');
+    correctAnswer = false;
+  }
 }
 
 if (nameAnswer === 'y'){
@@ -31,7 +38,14 @@ else {
 correctAnswer = false;
 while (!correctAnswer) {
   var birthAnswer = prompt('Was I born in Washington state?').toLowerCase();
-  correctAnswer = isCorrectFormat(birthAnswer);
+  let firstChar = birthAnswer.charAt(0);
+  if (firstChar === ('y') || firstChar === ('n')){
+    correctAnswer = true;
+  }
+  else {
+    alert('That is not a possible answer');
+    correctAnswer = false;
+  }
 }
 if (birthAnswer === 'y') {
   alert('Wrong, I was born in California.');
@@ -46,8 +60,14 @@ else {
 correctAnswer = false;
 while(!correctAnswer){
   var travelAnswer = prompt('Have I travelled internationally?').toLowerCase();
-  correctAnswer = isCorrectFormat(travelAnswer);
-}
+  let firstChar = travelAnswer.charAt(0);
+  if (firstChar === ('y') || firstChar === ('n')){
+    correctAnswer = true;
+  }
+  else {
+    alert('That is not a possible answer');
+    correctAnswer = false;
+  }}
 if (travelAnswer === 'y') {
   alert('Correct! I have travelled internationally many times');
 }
@@ -61,7 +81,14 @@ else {
 correctAnswer = false;
 while(!correctAnswer){
   var roleplayingAnswer = prompt('Is D&D my favorite roleplaying system?').toLowerCase();
-  correctAnswer = isCorrectFormat(roleplayingAnswer);
+  let firstChar = roleplayingAnswer.charAt(0);
+  if (firstChar === ('y') || firstChar === ('n')){
+    correctAnswer = true;
+  }
+  else {
+    alert('That is not a possible answer');
+    correctAnswer = false;
+  }
 }
 if (roleplayingAnswer === 'y') {
   alert('Wrong, D&D is one of my least favorite roleplaying systems I have actually played.');
@@ -76,7 +103,14 @@ else {
 correctAnswer = false;
 while(!correctAnswer){
   var numCatsAnswer = prompt('Does my family have only one cat?').toLowerCase();
-  correctAnswer = isCorrectFormat(numCatsAnswer);
+  let firstChar = numCatsAnswer.charAt(0);
+  if (firstChar === ('y') || firstChar === ('n')){
+    correctAnswer = true;
+  }
+  else {
+    alert('That is not a possible answer');
+    correctAnswer = false;
+  }
 }
 if (numCatsAnswer === 'y') {
   alert('Wrong, there are five total cats in my immediate family, three in Washington and two in California.');
