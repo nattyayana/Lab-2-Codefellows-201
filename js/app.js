175c84c26ba75036013333b4dae229d9fa07158e
nattyayana <natnalezayana@gmail.com>
2018-03-08 14:37:01 -0800

    added question, validate, answer, funcations

--- js/app.js
+++ js/app.js
@@ -1,6 +1,66 @@
 'use strict';
+//check if userAnswer is in the correct format
+function getUserResponse(question) {
+  var userResponse= prompt(question);
+  return userResponse; 
+
+}
+function validateAnswer(answerType, answer, array = null ) {
+  var correctFormat = false;
+
+
+  
+  if (answerType === 'yes/no'){
+    answer = answer.toLowerCase().charAt(0);
+    if (answer === 'y' || answer === 'n'){
+      correctFormat = true;
+    }
+  }
+  else if (answerType === 'number'){
+    answer = parseInt(answer);
+    if (isNaN(answer)){
+      correctFormat = false;
+    }
+    else {
+      correctFormat = true;
+    }
+  }
+  else if (answerType === 'array'){
+    for (let n = 0; n < array.length; n++){
+      if (answer === array[n]){
+        correctFormat = true;
+        break;
+      }
+    }
+  }
+  return correctFormat;
+}
+function answer(userAnswer, wrongResponse, correctResponse, answer, correctQuestions,answerType, counter = 0)  {
+  if (userAnswer === answer){
+    correctQuestions++;
+    alert(correctResponse);
+    return [true, correctQuestions]; 
+  }
+  else {
+    alert(wrongResponse);
+    if (answerType === 'number' && counter > 0){
+      alert('You have ' + counter + ' guesses left.');
+      return [false, correctQuestions]; 
+    }
+    else if (answerType === 'array' && counter > 0){
+      alert('You have ' + counter + ' guesses left.');
+      return [false, correctQuestions];
+    }
+    else {
+      return [true, correctQuestions]; 
+    
+  }
+
+}
+
 
 var correctQuestions = 0;
+
 
 //this is a set of arrays that are related to the questions asked of the user
 
@@ -10,45 +70,23 @@ var questionArray = ['Is my name Alex?', 'Was I born in Washington state?', 'Hav
 var answerArray = ['y', 'n', 'y', 'n', 'n', 0, 'Sweden'];
 
 
-var answerTypeArray = ['yes/no', 'yes/no', 'yes/no', 'yes/no', 'yes/no', 'number', 'country-array'];
+var answerTypeArray = ['yes/no', 'yes/no', 'yes/no', 'yes/no', 'yes/no', 'number', 'array'];
 var correctResponse = ['Yes it is!', 'Correct! I was born in California.', 'Yes, many times.', 'It is actually one of my least favorite.', 'Yes we own five.', 'Correct, we\'ve never owned dogs while I\'ve been alive', 'Correct, but I would like to go there someday.'];
 var wrongResponse = ['Actually it is Alex(Legally it\'s Alexander', 'No, I was born in California', 'Acutally I\'ve been out of the country quite often', 'No, I actually greatly dislike it.', 'We actually own five cats', 'No, not that many dogs.', 'Wrong, I have been there before.'];
 
 var countryArray = ['Argentina', 'Australia', 'Canada', 'Italy', 'Sweden', 'Peru', 'United Kingdom'];
+
+
+
 
 //this is a set of counters for the loops
 var i = 0;
 var numberCounter = 4;
 var countryCounter = 6;
 
-while (i < questionArray.length){
-  let correctFormat = false;
-  var userAnswer = prompt(questionArray[i]);
 
-  //check if userAnswer is in the correct format
-  if (answerTypeArray[i] === 'yes/no'){
-    userAnswer = userAnswer.toLowerCase().charAt(0);
-    if (userAnswer === 'y' || userAnswer === 'n'){
-      correctFormat = true;
-    }
-  }
-  else if (answerTypeArray[i] === 'number'){
-    userAnswer = parseInt(userAnswer);
-    if (isNaN(userAnswer)){
-      correctFormat = false;
-    }
-    else {
-      correctFormat = true;
-    }
-  }
-  else if (answerTypeArray[i] === 'country-array'){
-    for (let n = 0; n < countryArray.length; n++){
-      if (userAnswer === countryArray[n]){
-        correctFormat = true;
-        break;
-      }
-    }
-  }
+while (i < questionArray.length){
+
 
   //check userAnswer if correct format
   if (correctFormat){
