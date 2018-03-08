949fb7af330b51f5ae5afb61d5956f7a0f07fc01
Alexander Stone <aloecend@gmail.com>
2018-03-07 16:13:45 -0800

    removed trailing space on line 32.

--- js/app.js
+++ js/app.js
@@ -29,7 +29,7 @@ for (let i = 0; i < questionArray.length; i++){
     if (userAnswer === 'y' || userAnswer === 'n'){
       correctFormat = true;
     }
-  } 
+  }
   else if (answerTypeArray[i] === 'number'){
     userAnswer = parseInt(userAnswer);
     if (isNaN(userAnswer)){
